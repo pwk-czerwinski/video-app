46354c9009bb4f299367172bef6859eb06b90a19
pwk-czerwinski <pwkczerwinski.work@gmail.com>
2020-06-26 19:53:19 +0200

    Clean up

--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
-import Video from "./Video";
-import SrApi from "../api/SrApi";
+import Video from './Video';
+import SrApi from '../api/SrApi';
 
 function VideoList() {
     const [ movies, setMovies ] = useState([]);
