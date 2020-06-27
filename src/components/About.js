3f2f98d6e32c20a1c8a599490b4440763017b4c9
pwk-czerwinski <pwkczerwinski.work@gmail.com>
2020-06-27 15:49:47 +0200

    Implemented component with information about application

---
+++ src/components/About.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <div className="container">
+            <h2>Correct place to store your favourite youtube videos</h2>
+            <p>
+                This application give you the possibility to add, edit or delete chosen video.
+                After adding videos you will see these videos on the home page after
+                entering the application.
+                <strong> Not wait - try now!</strong>
+            </p>
+        </div>
+    )
+};
+
+export default About;
