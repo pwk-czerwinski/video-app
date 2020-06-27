f32605e80284c59b6549c6cd55b689f5f0a3e223
pwk-czerwinski <pwkczerwinski.work@gmail.com>
2020-06-27 15:55:54 +0200

    Implemented delete movie function and added required links to show details about specific video or edit it

--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -1,9 +1,18 @@
 import React, { useState, useEffect } from 'react';
 import Video from './Video';
 import SrApi from '../api/SrApi';
+import {NavLink} from 'react-router-dom';
 
 function VideoList() {
     const [ movies, setMovies ] = useState([]);
+
+    const deleteMovie = id => {
+        SrApi.deleteMovie(id);
+        SrApi.getMovies().then(movies => {
+            movies = movies.filter(movie => movie.video_url.includes('youtube') && movie.video_url.length > 20);
+            setMovies(movies)
+        });
+    }
 
     useEffect(() => {
         SrApi.getMovies().then(movies => {
@@ -13,15 +22,24 @@ function VideoList() {
     }, []);
 
     return(
-        <div className="content">
-            { movies.map(movie =>
-                <Video
-                    key={movie.id }
-                    title={movie.title}
-                    description={movie.description}
-                    url={movie.video_url.replace('watch?v=', 'embed/')}
-                />)
-            }
+        <div className="container">
+            <div className="content">
+                { movies.map(movie =>
+                    <div key={movie.id }>
+                        <Video
+                            title={movie.title}
+                            description={movie.description}
+                            url={movie.video_url.replace('watch?v=', 'embed/')}
+                            width={320}
+                            height={240}
+                        />
+                        <NavLink to={`/video/${movie.id}`}><button>More Details</button></NavLink><br/>
+                        <NavLink to={`/video/${movie.id}/update`}><button>Edit</button></NavLink><br/>
+                        <button onClick={() => deleteMovie(movie.id)}>Delete</button>
+                    </div>
+                    )
+                }
+            </div>
         </div>
     );
 }
