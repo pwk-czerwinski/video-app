b42efd056d6cd308827a4a5c6fa39372139d6b30
pwk-czerwinski <pwkczerwinski.work@gmail.com>
2020-06-27 15:56:30 +0200

    Implemented component for showing details about specific video

---
+++ src/components/VideoDetails.js
@@ -0,0 +1,32 @@
+import React, {useEffect, useState} from 'react';
+import Video from './Video';
+import SrApi from '../api/SrApi';
+
+const VideoDetails = (props) => {
+    const [title, setTitle] = useState('');
+    const [description, setDescription] = useState('');
+    const [videoUrl, setVideoUrl] = useState('');
+
+    useEffect(() => {
+        SrApi.getMovie(props.match.params.id).then(movie => {
+            setTitle(movie.title);
+            setDescription(movie.description);
+            console.log(movie.video_url);
+            setVideoUrl(movie.video_url);
+        });
+    }, [props.match.params.id]);
+
+    return (
+        <div className="container">
+            <Video
+                title={title}
+                description={description}
+                url={videoUrl}
+                width={960}
+                height={720}
+            />
+        </div>
+    );
+}
+
+export default VideoDetails;
